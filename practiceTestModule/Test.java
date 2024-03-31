b91a4287fa927f71eba6c1dd4420f2484072542f
meencodewriter <meencoding@gmail.com>
2024-04-01 00:03:01 +0900

    테스트 모듈 > 테스트클래스 생성

---
+++ practiceTestModule/Test.java
@@ -0,0 +1,10 @@
+package practiceTestModule;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan.next());
+    }
+}
