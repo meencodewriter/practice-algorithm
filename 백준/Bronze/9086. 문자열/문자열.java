222c6f8cd8475703a5d1e9641252e40f75374ddd
meencodewriter <126137098+meencodewriter@users.noreply.github.com>
2024-04-15 14:53:15 +0900

    [Bronze V] Title: 문자열, Time: 144 ms, Memory: 15920 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            System.out.println(s.charAt(0) + "" + s.charAt(s.length()-1));
+        }
+    }
+}
